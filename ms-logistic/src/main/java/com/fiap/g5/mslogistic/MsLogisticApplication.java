18ef7ade687cec4eac599d9f0ac184190acf19bf
AllrightIsHere <gasilva912@gmail.com>
2025-02-17 16:35:45 -0300

    feat: add new structure

---
+++ ms-logistic/src/main/java/com/fiap/g5/mslogistic/MsLogisticApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.g5.mslogistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLogisticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsLogisticApplication.class, args);
+    }
+
+}
---
+++ ms-logistic/src/test/java/com/fiap/g5/mslogistic/MsLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.g5.mslogistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLogisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
