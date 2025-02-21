71b3c3899349ac3269e66ecfa266db18f6e56df7
AllrightIsHere <gasilva912@gmail.com>
2025-02-21 10:00:54 -0300

    feat: add migrations

---
+++ ms-logistic/db/migration/V0001_create_database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `ms_logistic`.`logistic` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `postcode` VARCHAR(150) NOT NULL,
+  `number` INT NOT NULL,
+  `order_id` INT NOT NULL,
+  `status` VARCHAR(150) NOT NULL,
+  `delivery` VARCHAR(150) NOT NULL,
+  `estimated_date` DATETIME(6) NOT NULL,
+  `delivery_date` DATETIME(6) NULL,
+  PRIMARY KEY (`id`)
+);
